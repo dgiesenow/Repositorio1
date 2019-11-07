a0a59240398fd0e836f1d8ae95a72267a7a9d9f5
maiteGalindez <galindezmaite@gmail.com>
2019-11-07 18:18:09 -0300

    buscador

--- Javascript/ resultados.js
+++
@@ -1,6 +0,0 @@
-
-var imgsContainer = document.querySelector('#imgs_container');
-
-var queryString = new URLSearchParams(location.search);
-
-var url = ""// aca va el link con el api key
--- js/buscador.js
+++ js/buscador.js
@@ -1,25 +1,43 @@
 var apikey= "6695f769d740495966218b5ad75558be";
 
-var buscador = document.querySelector('#searchForm');
-var campoPalabra = document.querySelector('#search_word');
-var campoCategorias = document.querySelector('#categories');
+window.onload =function () {
 
-var palabraDeCategoria = '';
+ var buscador= document.querySelector ('.searchForm');
+ URLSearchParams
 
-campoCategorias.onchange = function () {
-	console.log(this.value);
-	palabraDeCategoria = this.value;
-	campoPalabra.value = '';
 }
 
-campoPalabra.onchange = function () {
-	console.log(this.value);
-	palabraDeCategoria = this.value;
-}
 
-buscador.onsubmit = function (event) {
-	if (palabraDeCategoria.length == 0) {
-		alert('Campos vacíos');
-		event.preventDefault();
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
