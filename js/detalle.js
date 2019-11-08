25f4d023b54eab949f7b753c1e0d4b1f01440114
delfigiesenow <dgiesenow@udesa.edu.ar>
2019-11-08 11:45:12 -0300

    punj

---
+++ js/detalle.js
@@ -0,0 +1,45 @@
+
+var parametrosURL = new URLSearchParams(location.search);
+console.log(parametrosURL.get('idSerie'));
+
+var idSerie = parametrosURL.get('idSerie')
+
+var url = `https://api.themoviedb.org/3/tv/${idSerie}?api_key=6695f769d740495966218b5ad75558be&language=en-US`;
+
+fetch (url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (informacion) {
+    console.log(informacion);
+    // Aca haces las cosas
+    
+  })
+  .catch(function (errors) {
+      console.log(errors);
+  });
+  //
+  //
+  // fetch(url)
+  //   .then(function (res) {
+  //     return res.json();
+  //   })
+  //   .then(function (informacion) {
+  //     console.log(informacion.results);
+  //     for (var i = 0; i < informacion.results.length; i++) {
+  //       informacion.results[i]
+  //       console.log(informacion.results[i].name);
+  //       console.log(informacion.results[i].id);
+  //       console.log(informacion.results[i].poster_path);
+  //       console.log(informacion.results[i].overview);
+  //
+  //       var buscadorGeneral = document.querySelector (".paginadetalles")
+  //       var elementsHtml ='<div class=" uk-card uk-card-default uk-card-body uk-width-1-5">'
+  //        elementsHtml +='<img src="https://image.tmdb.org/t/p/original' + informacion.results[i].poster_path + informacion.results[i].overview + console.log(informacion.results[i].name>'
+  //        elementsHtml +='</div> '
+  //        buscadorGeneral.innerHTML += elementsHtml
+  //     }
+  //     })
+  //   .catch(function (errors) {
+  //     console.log(errors);
+  //   });
--- js/index.js
+++ js/index.js
@@ -4,28 +4,36 @@ window.onload= function () {
   		return res.json();
   	})
   	.then(function (informacion) {
-      console.log(informacion.results);
-      for (var i = 0; i < informacion.results.length; i++) {
-        informacion.results[i]
-        console.log(informacion.results[i].name);
-        console.log(informacion.results[i].id);
-        console.log(informacion.results[i].poster_path);
 
-        var ul = document.querySelector (".populares")
-        var elementsHtml ='<li>'
-         elementsHtml +='<div class="uk-panel">'
-         elementsHtml +='<img src="https://image.tmdb.org/t/p/original' + informacion.results[i].poster_path + '" elements>'
-         elementsHtml +='</div> </li>'
-         ul.innerHTML += elementsHtml
+      // console.log(informacion.results); Esto es para que funcione el detalle.  
+      var ul = document.querySelector(".populares")
+      var elementsHtml = '';
+
+      for (var i = 0; i < informacion.results.length; i++) {
+        // console.log(informacion.results[i].name);
+        // console.log(informacion.results[i].id);
+        // console.log(informacion.results[i].poster_path);
+         elementsHtml += `
+          <li>
+            <div class="uk-panel">
+              <a href="detalle.html?idSerie=${informacion.results[i].id}">
+                <img src="https://image.tmdb.org/t/p/original/${informacion.results[i].poster_path}" elements>
+              </a>
+            </div>
+          </li>
+        `;
+
       }
-    	})
+
+      ul.innerHTML = elementsHtml;
+    })
   	.catch(function (errors) {
   		console.log(errors);
   	});
 
 
 
-
+// top rated
   fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=6695f769d740495966218b5ad75558be&language=en-US&page=1")
   	.then(function (res) {
   		return res.json();
@@ -49,6 +57,9 @@ window.onload= function () {
   	.catch(function (errors) {
   		console.log(errors);
   	});
+
+    // popular
+
     fetch("https://api.themoviedb.org/3/tv/popular?api_key=6695f769d740495966218b5ad75558be&language=en-US&page=1")
       .then(function (res) {
         return res.json();
@@ -72,6 +83,8 @@ window.onload= function () {
       .catch(function (errors) {
         console.log(errors);
       });
+
+  // On air
 
       fetch("https://api.themoviedb.org/3/tv/airing_today?api_key=6695f769d740495966218b5ad75558be&language=en-US&page=1")
         .then(function (res) {
